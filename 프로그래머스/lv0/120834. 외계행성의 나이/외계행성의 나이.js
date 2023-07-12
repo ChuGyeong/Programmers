470685bf231925d61123077ce87fe2042b215a06
ChuGyeong <121862169+ChuGyeong@users.noreply.github.com>
2023-07-12 09:33:18 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,20 @@
+function solution(age) {
+   const ageArr = {
+      0: 'a',
+      1: 'b',
+      2: 'c',
+      3: 'd',
+      4: 'e',
+      5: 'f',
+      6: 'g',
+      7: 'h',
+      8: 'i',
+      9: 'j',
+   };
+
+   return age
+      .toString()
+      .split('')
+      .map(item => ageArr[item])
+      .join('');
+}
