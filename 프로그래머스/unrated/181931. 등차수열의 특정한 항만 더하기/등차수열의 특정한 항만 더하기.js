b414b80bce89dba5752a848fe8447d8672dd7c13
ChuGyeong <121862169+ChuGyeong@users.noreply.github.com>
2023-09-05 19:36:56 +0900

    [unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -0,0 +1,8 @@
+function solution(a, d, included) {
+    const answer = [a]
+    for(let i = 1 ; i < included.length; i ++) {
+        answer[i] = answer[i-1] + d
+    }
+    
+    return answer.reduce((acc, cur, idx) => included[idx] ? acc+cur : acc , 0)
+}
