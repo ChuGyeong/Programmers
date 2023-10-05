b7b3ac7d796d7bd0fc3b64a04010e7cf4e37f86e
ChuGyeong <121862169+ChuGyeong@users.noreply.github.com>
2023-10-05 16:00:20 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,15 @@
+function solution(my_string) {
+    const answer = Array(52).fill(0);
+
+    for (let i = 0; i < my_string.length; i++) {
+        const charCode = my_string.charCodeAt(i);
+
+        if (charCode >= 65 && charCode <= 90) {
+            answer[charCode - 65] += 1;
+        } else if (charCode >= 97 && charCode <= 122) {
+            answer[charCode - 71] += 1;
+        }
+    }
+
+    return answer;
+}
