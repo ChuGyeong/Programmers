529baa80acd17fc0b8779a64bef50c34e77e3666
ChuGyeong <121862169+ChuGyeong@users.noreply.github.com>
2023-11-18 23:55:45 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+    let answer = [];
+    for (let i=0; i<commands.length; i++) {
+        let temp = array.slice(commands[i][0]-1 , commands[i][1]);
+        temp.sort(function(a, b) { return a-b });
+        answer.push(temp[commands[i][2]-1]);
+    }
+    return answer;
+}
