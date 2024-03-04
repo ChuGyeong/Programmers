533159e656643a278762892cb6958522bbaf97d2
ChuGyeong <121862169+ChuGyeong@users.noreply.github.com>
2024-03-05 08:21:59 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+function sort(number){
+  let answer = '';
+  answer = Number(number.toString().split("").sort((a,b)=>(b-a)).join(""));
+  
+  return answer;
+}
+
+console.log(sort(input))
